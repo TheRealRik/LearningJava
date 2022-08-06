21d2a51de8da4c12db49059a450b0c78d4da8940
The Real Rik <riki.naskar2019@outlook.com>
2022-08-06 19:51:33 +0530

    Upload II

--- src/gg/rikgames/www/method.java
+++ src/gg/rikgames/www/method.java
@@ -2,6 +2,25 @@ package gg.rikgames.www;
 
 public class method {
     public static void main(String[] args) {
+        class Human{
+            String name;
+            int age;
 
+            void talk(){
+                System.out.println("My name is " + name + " and I am " + age + " years old.");
+            }
+            int calculate(int one, int two, int three){
+                return one*two*three;
+            }
+        }
+        Human human1 = new Human();
+        Human human2 = new Human();
+
+        human1.name = "Rik The Noob";
+        human1.age = 15;
+
+        human1.talk();
+        System.out.println(human1.calculate(5,4,2));
+        System.out.println(human2.calculate(7,8,9));
     }
 }
