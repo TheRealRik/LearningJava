b7c5e65b464aa6a934d120d4a2b8a5869a110080
The Real Rik <riki.naskar2019@outlook.com>
2022-09-03 18:13:54 +0530

    Upload III

---
+++ src/gg/rikgames/www/palPrime.java
@@ -0,0 +1,30 @@
+package gg.rikgames.www;
+
+import java.util.Scanner;
+
+public class palPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, p, rev, s = 0, i, c = 0;
+        System.out.println("Enter No.");
+        n = scanner.nextInt();  // Input number from user
+        p = n; // store the entered number in "p" variable
+
+        for(i = 1; i <= p; i++) {
+            if (p % i == 0) {
+                c++;
+            }
+        }
+        while(n > 0) {
+            rev = n % 10; // extract last digit of the number
+            s = s * 10 + rev; // store the digit last digit
+            n = n / 10; // extract all digit except the last
+        }
+        if(p == s && c == 2) {       // comparing with original number
+            System.out.println("Number is PalPrime : "+p);
+        } else {
+            System.out.println("Number is not PalPrime : "+p);
+        }
+    }
+}
+
---
+++ src/gg/rikgames/www/technumber.java
@@ -0,0 +1,50 @@
+package gg.rikgames.www;
+
+import java.util.Scanner;
+
+public class technumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String mainString, string1, string2;
+        int length, userInput, a, b, sum, square;
+
+        System.out.println("|----------------------|");
+        System.out.println(" Please Enter A Number ");
+        System.out.println("|----------------------|");
+        //Putting the user given number into mainString variable
+        mainString = scanner.nextLine();
+
+        //Count the length of the user given number
+        length = mainString.length();
+        //Convert user given input string number into actual int
+        userInput = Integer.parseInt(mainString);
+
+        if ((length % 2) == 0) {
+            double division = 0.5;
+            division = (division * length);
+
+            //Dividing the user given number into two parts and putting them into two different String
+            string1 = mainString.substring(0, (int) division);
+            string2 = mainString.substring((int) division, length);
+
+            //Converting above two string into int
+            a = Integer.parseInt(string1);
+            b = Integer.parseInt(string2);
+
+            sum = (a + b);
+            square = (sum * sum);
+
+            if (userInput == square) {
+                System.out.println("*******************************");
+                System.out.println("    " + userInput + " Is A TECH Number    ");
+                System.out.println("*******************************");
+            } else {
+                System.out.println("*********************************");
+                System.out.println("    " + userInput + " Is Not A TECH Number    ");
+                System.out.println("*********************************");
+            }
+        } else{
+            System.out.println("Please Enter A 2, 4, 6, 8, 10 . . . Digit Number");
+        }
+    }
+}
